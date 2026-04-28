b7964b3cc38a072554ea7e5092d15597acdd2818
Tanmay khosla <tanmay@Tanmays-MacBook-Air.local>
2026-04-29 00:46:27 +0530

    Resume Add

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare module "gsap-trial/SplitText" {
+	export class SplitText {
+		constructor(targets: any, vars?: any);
+		chars: any[];
+		words: any[];
+		lines: any[];
+		revert(): void;
+	}
+}
