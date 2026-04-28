54401d8e748f486127b370929384642c2b6e130a
Tanmay khosla <tanmay@Tanmays-MacBook-Air.local>
2026-04-29 01:07:18 +0530

    fix: replace deprecated gsap-trial with standard gsap package (SplitText + ScrollSmoother now free)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,3 @@
 /// <reference types="vite/client" />
 
-declare module "gsap-trial/SplitText" {
-	export class SplitText {
-		constructor(targets: any, vars?: any);
-		chars: any[];
-		words: any[];
-		lines: any[];
-		revert(): void;
-	}
-}
+
